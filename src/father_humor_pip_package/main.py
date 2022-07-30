e8cf4e6a3c89a048d60afe618d724592125c0d6a
Anthony Hill <91757022+anthonymhill23@users.noreply.github.com>
2022-07-30 18:17:20 -0400

    changes variable names for star wars jokes

--- src/father_humor_pip_package/main.py
+++ src/father_humor_pip_package/main.py
@@ -5,8 +5,6 @@ from bs4 import BeautifulSoup
 
 
 def dad_jokes(url='https://www.countryliving.com/life/a27452412/best-dad-jokes/'):
-
-
     file_path = r'dad_jokes.text'
     jokelist = []
 
@@ -24,32 +22,35 @@ def dad_jokes(url='https://www.countryliving.com/life/a27452412/best-dad-jokes/'
             for joke in dad_joke:
                 jokelist.append(joke.text)
 
-    # create a file
+        # create a file
         with open(file_path, 'w') as fp:
             for joke in jokelist:
                 fp.write(joke + '\n')
+    return jokelist[0]
+
 
 def star_wars(url='https://www.littledayout.com/star-wars-jokes-puns-use-the-force-for-laughter/'):
     file_path = r'star_wars.text'
 
-    jokelist = []
+    starwars_jokelist = []
     if os.path.exists(file_path):
         print('file already exists')
     else:
         page = requests.get(url)
 
         soup = BeautifulSoup(page.content, 'html.parser')
-        sw_joke = soup.findAll(class_='td-post-content tagdiv-type')
+        sw_joke = soup.findAll(class_='td-post-content')
 
         for item in sw_joke:
             star_wars_joke = item.findAll('p')
             for joke in star_wars_joke:
-                jokelist.append(joke.text)
+                starwars_jokelist.append(joke.text)
 
-    # create a file
+        # create a file
         with open(file_path, 'w') as fp:
-            for joke in jokelist:
+            for joke in starwars_jokelist:
                 fp.write(joke + '\n')
+    return starwars_jokelist[0]
 
 
 def programming(url='https://www.ajokeaday.com/categories/programmer-jokes?page=1'):
@@ -69,15 +70,14 @@ def programming(url='https://www.ajokeaday.com/categories/programmer-jokes?page=
             for joke in programming_joke:
                 jokelist.append(joke.text)
 
-    # create a file
+        # create a file
         with open(file_path, 'w') as fp:
             for joke in jokelist:
                 fp.write(joke + '\n')
 
 
 if __name__ == '__main__':
-   programming()
-
+    programming()
 
 # numbers = range(0,5)
 # for num in numbers:
