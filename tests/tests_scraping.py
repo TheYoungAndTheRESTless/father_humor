728fc787bac48429dfd7069cdcb65f63d5ff3354
Anthony Hill <91757022+anthonymhill23@users.noreply.github.com>
2022-07-30 19:06:54 -0400

    tests completed

--- tests/tests_scraping.py
+++ tests/tests_scraping.py
@@ -1,19 +1,16 @@
 import pytest
 import os
-from src.father_humor_pip_package.main import dad_jokes,star_wars,programming
+from src.father_humor_pip_package.main import dad_jokes, star_wars, programming
 
-@pytest.mark.skip("testing proof of life")
-def test_proof_of_life():
-    sum = 1+1
-    expected = 2
-    assert sum == expected
+url = "https://scraping-test-site.brendonlh.repl.co/"
 
 
 # @pytest.mark.skip("testing proof of life")
 def test_proof_of_life():
-    sum = 1+1
+    sum = 1 + 1
     expected = 2
     assert sum == expected
+
 
 # @pytest.mark.skip("testing proof of life")
 def test_dad_jokes():
@@ -28,19 +25,33 @@ def test_dad_jokes():
         expected = '\'here is a fancy joke1 that is made of "li"tags'
         assert actual == expected
 
+
 # @pytest.mark.skip("testing proof of life")
 def test_star_wars():
     url = "https://scraping-test-site.brendonlh.repl.co/"
-    actual = star_wars(url)
-    expected = '\'here is a fancy joke1 that is made of "p"tags'
-    assert actual == expected
+    if os.path.exists("star_wars.text"):
+        os.remove("star_wars.text")
+        actual = star_wars(url)
+        expected = '\'here is a fancy joke1 that is made of "p"tags'
+        assert actual == expected
+    else:
+        actual = star_wars(url)
+        expected = '\'here is a fancy joke1 that is made of "p"tags'
+        assert actual == expected
+
 
 # @pytest.mark.skip("testing proof of life")
 def test_programming():
     url = "https://scraping-test-site.brendonlh.repl.co/"
-    actual = programming(url)
-    expected = '\'here is a programming joke1 that is made of "p"tags\''
-    assert actual == expected
+    if os.path.exists("programming.text"):
+        os.remove("programming.text")
+        actual = programming(url)
+        expected = '\'here is a programming joke1 that is made of "p"tags\''
+        assert actual == expected
+    else:
+        actual = programming(url)
+        expected = '\'here is a programming joke1 that is made of "p"tags\''
+        assert actual == expected
 
 # @pytest.mark.skip("File Test")
 
